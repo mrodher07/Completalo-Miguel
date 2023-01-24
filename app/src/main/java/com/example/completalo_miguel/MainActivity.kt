627048762c89171fc16843e3475039c987889d01
mrodher07 <miguelroher2002@gmail.com>
2023-01-24 14:38:49 +0100

    0.1

--- app/src/main/java/com/example/completalo_miguel/MainActivity.kt
+++ app/src/main/java/com/example/completalo_miguel/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.completalo_miguel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.SeekBar
+import android.widget.TextView
+import com.example.completalo_miguel.databinding.ActivityMainBinding
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var progresoX: TextView
+    private lateinit var progresoY: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sbNumeroColumnas.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+
+                updateXTiles(seekBar?.getProgress()!!)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        binding.sbNumeroFilas.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+
+                updateYTiles(seekBar?.getProgress()!!)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+
     }
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }*/
+
+    fun updateXTiles(progreso:Int) {
+        progresoX = binding.tvColumnas
+        progresoX.setText("Numero de Columnas : " + progreso)
+    }
+
+    fun updateYTiles(progreso:Int) {
+        progresoY = binding.tvFilas
+        progresoY.setText("Numero de Filas : " + progreso)
+    }
+
 }
